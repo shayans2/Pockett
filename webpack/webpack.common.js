b38df2b9c7fb4a37597bbeb076d18261e179e17c
Shayan Bemanian <shayan.bemanian@yahoo.com>
2021-12-26 00:06:44 +0330

    chore: set stats to minimal

--- webpack/webpack.common.js
+++ webpack/webpack.common.js
@@ -3,7 +3,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   entry: path.resolve(__dirname, '..', './src/index.js'),
-  // stats: 'minimal',
+  stats: 'minimal',
   module: {
     rules: [
       {
