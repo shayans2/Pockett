22350bb7c236fc6170f8dd2472c7b3111cbcf4bf
Shayan Bemanian <shayan.bemanian@yahoo.com>
2021-12-27 00:46:36 +0330

    feat: add support for svg

--- webpack/webpack.common.js
+++ webpack/webpack.common.js
@@ -15,6 +15,10 @@ module.exports = {
         test: /\.css$/i,
         use: ['style-loader', 'css-loader'],
       },
+      {
+        test: /\.(png|jpg|svg)/,
+        type: 'asset/resource',
+      },
     ],
   },
   plugins: [
