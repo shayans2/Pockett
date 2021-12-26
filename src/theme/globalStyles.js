674d80d246e8695483bbd2ef21c4ea12345af557
Shayan Bemanian <shayan.bemanian@yahoo.com>
2021-12-27 00:36:15 +0330

    fix: move gfonts to html for build problems

--- src/theme/globalStyles.js
+++ src/theme/globalStyles.js
@@ -2,7 +2,6 @@ import { createGlobalStyle } from 'styled-components';
 
 export const GlobalStyles = createGlobalStyle`
   body {
-    @import url('https://fonts.googleapis.com/css2?family=Montserrat:wght@100;200;300;400;500;600;800;900&display=swap');
     margin: 0;
     padding: 0;
     box-sizing: border-box;
