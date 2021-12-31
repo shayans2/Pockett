6da2d37e5ee796db7640ae080b4e9b2f32026808
Shayan Bemanian <shayan.bemanian@yahoo.com>
2021-12-31 22:45:53 +0330

    feat: prevent text selection and zooming

--- src/theme/globalStyles.js
+++ src/theme/globalStyles.js
@@ -10,5 +10,11 @@ export const GlobalStyles = createGlobalStyle`
     box-sizing: border-box;
     font-family: "Montserrat", Tahoma;
     background-color: #14242a;
+    -webkit-touch-callout: none;
+    -webkit-user-select: none;
+    -khtml-user-select: none; 
+    -moz-user-select: none;
+    -ms-user-select: none; 
+     user-select: none; 
   }
 `;
