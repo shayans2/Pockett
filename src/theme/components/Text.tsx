b06570e992a72f93a0dd3b3695285600fe96d3c4
Shayan Bemanian <shayan.bemanian@yahoo.com>
2022-01-16 19:04:09 +0330

    fix: make properties fully optional

--- src/theme/components/Text.tsx
+++ src/theme/components/Text.tsx
@@ -2,19 +2,19 @@ import styled from 'styled-components';
 import { themeConfig } from '../config';
 
 interface TextProps {
-  color: keyof typeof themeConfig.colors;
-  size: keyof typeof themeConfig.fontSize;
-  weight: keyof typeof themeConfig.fontWeight;
+  color?: keyof typeof themeConfig.colors;
+  size?: keyof typeof themeConfig.fontSize;
+  weight?: keyof typeof themeConfig.fontWeight;
   lh?: string;
-  align: 'left' | 'center' | 'right';
+  align?: 'left' | 'center' | 'right';
   margin?: string;
   padding?: string;
 }
 
 export const Text = styled.p<TextProps>`
-  color: ${({ theme, color }) => theme.colors[color]};
-  font-size: ${({ theme, size }) => theme.fontSize[size]};
-  font-weight: ${({ theme, weight }) => theme.fontWeight[weight]};
+  color: ${({ theme, color }) => theme.colors[color!]};
+  font-size: ${({ theme, size }) => theme.fontSize[size!]};
+  font-weight: ${({ theme, weight }) => theme.fontWeight[weight!]};
   line-height: ${({ lh }) => lh};
   text-align: ${({ align }) => align};
   margin: ${({ margin }) => margin};
@@ -22,6 +22,9 @@ export const Text = styled.p<TextProps>`
 `;
 
 Text.defaultProps = {
+  color: 'white',
+  size: 'regular',
+  weight: 'regular',
   margin: '0',
   padding: '0',
 };
